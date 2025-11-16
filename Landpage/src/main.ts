739c698d2dacefe30a5d16f7f39bae913829d1bc
LucasSantino <lsantino541@gmail.com>
2025-11-16 11:20:07 -0300

    update

--- Landpage/src/main.ts
+++ Landpage/src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
+import router from './router'
+import { createPinia } from 'pinia'
+import 'material-icons/iconfont/material-icons.css'
+import 'primeflex/primeflex.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(createPinia())
+
+app.mount('#app')
---
+++ Landpage/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Home View funcionando!</h1>
+</template>
+
+<script setup lang="ts">
+</script>
